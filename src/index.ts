3e3d77653308189a036694c75800fcba97a51f56
Paul Pan <panjunhong05@gmail.com>
2021-01-05 01:03:50 -0800

    Code cleanup.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import HomePage from './assets/html/index.html';
 
 export async function handleRequest(request: Request): Promise<Response> {
-  console.log(new URL(request.url).pathname);
   switch (new URL(request.url).pathname) {
     case '/':
       return new Response(HomePage, {
         headers: { 'content-type': 'text/html' },
       });
-    case '/favicon.ico':
-      return new Response();
     default:
       return new Response()
   }
