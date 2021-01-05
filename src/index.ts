b8390380ba9ba5130485ca6ca3699ba53e9f8383
Paul Pan <panjunhong05@gmail.com>
2021-01-05 01:07:28 -0800

    Code formatting.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import HomePage from './assets/html/index.html';
+import HomePage from './assets/html/index.html'
 
 export async function handleRequest(request: Request): Promise<Response> {
   switch (new URL(request.url).pathname) {
     case '/':
       return new Response(HomePage, {
         headers: { 'content-type': 'text/html' },
-      });
+      })
     default:
       return new Response()
   }
